fcdd540e8aa5f4e1414bd614137429d94d6abc42
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2019-10-30 14:47:57 +0100

    Adding model and repository elements.

--- SportsStore/SportsStore/Startup.cs
+++ SportsStore/SportsStore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SportsStore.Models;
 
 namespace SportsStore
 {
@@ -17,6 +18,8 @@ namespace SportsStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddTransient<IProductRepository, FakeProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
