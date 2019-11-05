39df7a7136e2dcad1964d86807c3e8d8be6d5091
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2019-11-05 13:21:57 +0100

    Adding a tag helper for the pagination feature. Plus, adding a unit test prior the tag helper full implementation.

---
+++ SportsStore/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Infrastructure
+{
+    public class PageLinkTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory _urlHelperFactory;
+
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        public PageLinkTagHelper(IUrlHelperFactory urlHelperFactory)
+        {
+            _urlHelperFactory = urlHelperFactory;
+        }
+    }
+}
