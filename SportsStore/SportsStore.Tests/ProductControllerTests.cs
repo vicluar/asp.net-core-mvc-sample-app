16b2dae132bda014fd9753a99e784d07fcca0af5
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2019-11-18 15:42:32 +0100

    Adding a new unit test for filtering products.

--- SportsStore/SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore/SportsStore.Tests/ProductControllerTests.cs
@@ -48,19 +48,43 @@ namespace SportsStore.Tests
 
         [Fact]
         public void Can_Send_Pagination_View_Model()
-        {            
+        {
             // Arrange
             var sut = new ProductController(_mockProductRepository.Object) { PageSize = 3 };
-            
+
             // Act
             var result = sut.List(null, 2).ViewData.Model as ProductsListViewModel;
-            
+
             // Assert
             PagingInfo pageInfo = result.PagingInfo;
             Assert.Equal(2, pageInfo.CurrentPage);
             Assert.Equal(3, pageInfo.ItemsPerPage);
             Assert.Equal(5, pageInfo.TotalItems);
             Assert.Equal(2, pageInfo.TotalPages);
+        }
+
+        [Fact]
+        public void Can_Filter_Products()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+                }).AsQueryable());
+            
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            
+            Product[] result =
+            (controller.List("Cat2", 1).ViewData.Model as ProductsListViewModel)
+            .Products.ToArray();
+
+            Assert.Equal(2, result.Length);
+            Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
     }
 }
