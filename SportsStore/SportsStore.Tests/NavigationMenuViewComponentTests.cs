f41879e2317bf75d4a8841ce30deedb22928f089
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2020-01-15 16:08:47 +0100

    Creating a new unit test class to test the menu view component.

---
+++ SportsStore/SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class NavigationMenuViewComponentTests
+    {
+        [Fact]
+        public void Can_Select_Categories()
+        {
+
+        }
+    }
+}
