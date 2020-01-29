665e5c5781b27884c0c4d5042402b717bf2901a4
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2020-01-29 15:36:20 +0100

    Adding a new unit test for using the selected category. Applying green-read unit testing approach. Plus, fixing a small bug on previous unit test for this class.

--- SportsStore/SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore/SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Routing;
 using Moq;
 using SportsStore.Components;
 using SportsStore.Models;
@@ -20,9 +22,9 @@ namespace SportsStore.Tests
                 new Product[]
                 {
                     new Product { ProductID = 1, Name = "Product1", Category = "Apples" },
-                    new Product { ProductID = 1, Name = "Product1", Category = "Apples" },
-                    new Product { ProductID = 1, Name = "Product1", Category = "Apples" },
-                    new Product { ProductID = 1, Name = "Product1", Category = "Apples" }
+                    new Product { ProductID = 2, Name = "Product2", Category = "Oranges" },
+                    new Product { ProductID = 3, Name = "Product3", Category = "Apples" },
+                    new Product { ProductID = 4, Name = "Product4", Category = "Plums" }
                 }).AsQueryable());
 
             var sut = new NavigationMenuViewComponent(mockRepository.Object);
@@ -30,6 +32,36 @@ namespace SportsStore.Tests
             var result = ((IEnumerable<string>)(sut.Invoke() as ViewViewComponentResult).ViewData.Model).ToArray();
 
             Assert.True(Enumerable.SequenceEqual(new string[] { "Apples", "Oranges", "Plums" }, result));
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            var categoryToSelect = "Apples";
+
+            var mockRepository = new Mock<IProductRepository>();
+            mockRepository.Setup(m => m.Products).Returns((
+                new Product[]
+                {
+                    new Product { ProductID = 1, Name = "Product1", Category = "Apples" },
+                    new Product { ProductID = 4, Name = "Product4", Category = "Oranges" }
+                }
+                ).AsQueryable());
+
+            var sut = new NavigationMenuViewComponent(mockRepository.Object);
+
+            sut.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new RouteData()
+                }
+            };
+            sut.RouteData.Values["category"] = categoryToSelect;
+
+            var result = (sut.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"].ToString();
+
+            Assert.Equal(categoryToSelect, result);
         }
     }
 }
