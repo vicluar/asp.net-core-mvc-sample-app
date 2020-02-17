bc306f50b1681604f6ee8cf6e0f64dd3f86fec37
Vicluar Daniel Merchan Briceno <vicluar@gmail.com>
2020-02-17 15:44:33 +0100

    Adding an url extensions class to help on the query string when getting back. Plus, adding the add to cart button in the product summary partial view.

---
+++ SportsStore/SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest httpRequest)
+        {
+            return httpRequest.QueryString.HasValue ? $"{httpRequest.Path}{httpRequest.QueryString}" :
+                httpRequest.Path.ToString();
+        }
+    }
+}
